da0ba2199089f0978f0bd4cc321229aeef64f7c0
Delane <delanebrandy13@outlook.com>
2023-04-17 14:39:27 +0100

    func chanages

--- main.py
+++ main.py
@@ -16,236 +16,253 @@ def printType(inVar):
 
 
 #Data types in python 
-a = "Hello World" # str
-b = 20	# int
-c = 20.5 # float, decimal point or exponent notation
-d = 1j # complex number
-e = True # boolean 
-e_2 = False # boolean
+def dataTypes():
+    a = "Hello World" # str
+    b = 20	# int
+    c = 20.5 # float, decimal point or exponent notation
+    d = 1j # complex number
+    e = True # boolean 
+    e_2 = False # boolean
 
-list = [a, b, c, d, e, e_2] # list
+    list = [a, b, c, d, e, e_2] # list
 
-printType(a)
-printTypesList(list)
-
+    printType(a)
+    printTypesList(list)
 
 #Variables in python
+def variables():
 
-num1 = 56
-num2 = 12
+    num1 = 56
+    num2 = 12
 
-num3 = num1 + num2
+    num3 = num1 + num2
 
-string1 = "God i want him so bad"
-string2 = "Hi, my name is noel"
+    string1 = "God i want him so bad"
+    string2 = "Hi, my name is noel"
 
-string3 = string1 + " " + string2 
+    string3 = string1 + " " + string2 
 
-float1 = 1.5
+    float1 = 1.5
 
-mixed = num3 + float1
+    mixed = num3 + float1
 
-same = 10
-value = 10
+    same = 10
+    value = 10
 
 
-#prints
+    #prints
 
-print(num3)
-print(string3)
-print(mixed)
-print(same == value) # True
-print(same == 12)
-print("\n")
+    print(num3)
+    print(string3)
+    print(mixed)
+    print(same == value) # True
+    print(same == 12)
+    print("\n")
 
 ##Operators in python
 #Arithmetic: + - * / % ** //
+def operators():
+    x = 10
+    y = 3
+    print(x + y)  # addition operator
+    print(x - y)  # subtraction operator
+    print(x * y)  # multiplication operator
+    print(x / y)  # division operator
+    print(x % y)  # modulo operator (remainder)
+    print(x ** y) # exponent operator
 
-x = 10
-y = 3
-print(x + y)  # addition operator
-print(x - y)  # subtraction operator
-print(x * y)  # multiplication operator
-print(x / y)  # division operator
-print(x % y)  # modulo operator (remainder)
-print(x ** y) # exponent operator
+    #fun one
 
-#fun one
+    x = 10.56
+    y = 2
 
-x = 10.56
-y = 2
+    print(x // y) # floor division operator (rounds down[integer division])
 
-print(x // y) # floor division operator (rounds down[integer division])
+    #Comparison  == != > < >= <=
 
-#Comparison  == != > < >= <=
+    x = 10
+    y = 5
+    print(x > y)   # greater than operator
+    print(x < y)   # less than operator
+    print(x == y)  # equality operator
+    print(x != y)  # not equal operator
+    print(x >= y)  # greater than or equal to operator
+    print(x <= y)  # less than or equal to operator
 
-x = 10
-y = 5
-print(x > y)   # greater than operator
-print(x < y)   # less than operator
-print(x == y)  # equality operator
-print(x != y)  # not equal operator
-print(x >= y)  # greater than or equal to operator
-print(x <= y)  # less than or equal to operator
+    #Logical 
 
-#Logical 
+    x = True
+    y = False
+    print(x and y) # logical AND operator
+    print(x or y)  # logical OR operator
+    print(not x)   # logical NOT operator
 
-x = True
-y = False
-print(x and y) # logical AND operator
-print(x or y)  # logical OR operator
-print(not x)   # logical NOT operator
+    #Assignment 
 
-#Assignment 
+    x = 10
+    x += 5  # equivalent to x = x + 5
+    print(x)
 
-x = 10
-x += 5  # equivalent to x = x + 5
-print(x)
-
-y = 3
-y *= 2  # equivalent to y = y * 2
-print(y)
+    y = 3
+    y *= 2  # equivalent to y = y * 2
+    print(y)
 
 
 ##Conditional statements
 # if, elif, else
 
+
+def conditions():
 #if statement:
-x = 10
-if x > 5:
-    print("x is greater than 5")
+    x = 10
+    if x > 5:
+        print("x is greater than 5")
 
-#If-else statement:
-x = 3
-if x > 5:
-    print("x is greater than 5")
-else:
-    print("x is less than or equal to 5")
+    #If-else statement:
+    x = 3
+    if x > 5:
+        print("x is greater than 5")
+    else:
+        print("x is less than or equal to 5")
 
-#If-elif-else statement:
-x = 7
-if x < 0:
-    print("x is negative")
-elif x == 0:
-    print("x is zero")
-else:
-    print("x is positive")
+    #If-elif-else statement:
+    x = 7
+    if x < 0:
+        print("x is negative")
+    elif x == 0:
+        print("x is zero")
+    else:
+        print("x is positive")
 
-#Nested if statements
-x = 10
-if x > 5:
-    if x < 15:
-        print("x is between 5 and 15")
+    #Nested if statements
+    x = 10
+    if x > 5:
+        if x < 15:
+            print("x is between 5 and 15")
+            
+
+    #all combined example
+
+    value = int(input("Enter a number: "))
+    if value > 0:
         
-
-#all combined example
-
-value = int(input("Enter a number: "))
-if value > 0:
-    
-    print("x is positive")
-    
-    if value < 50:
-        print("x is a small positive number")
+        print("x is positive")
         
-    elif value < 100:
-        print("x is a medium positive number")
+        if value < 50:
+            print("x is a small positive number")
+            
+        elif value < 100:
+            print("x is a medium positive number")
+            
+        else:
+            print("x is a large positive number")
+            
+    elif value == 0:
+        print("x is zero")
+        
+    elif value < 0:
+        print("x is negative")
         
     else:
-        print("x is a large positive number")
-        
-elif value == 0:
-    print("x is zero")
-    
-elif value < 0:
-    print("x is negative")
-    
-else:
-    print("x is not a number")
+        print("x is not a number")
     
 ##Loops
+def loops():
+    fruits = ["apple", "banana", "cherry"]
+    for x in fruits:
+        print(x)
 
-fruits = ["apple", "banana", "cherry"]
-for x in fruits:
-    print(x)
+    i = 1
+    while i < 6:
+        print(i)
+        i += 1 # i = i + 1
 
-i = 1
-while i < 6:
-    print(i)
-    i += 1 # i = i + 1
+    adj = ["red", "big", "tasty"]
+    fruits = ["apple", "banana", "cherry"]
 
-adj = ["red", "big", "tasty"]
-fruits = ["apple", "banana", "cherry"]
+    for x in adj:
+        for y in fruits:
+            print(x, y)
 
-for x in adj:
-    for y in fruits:
-        print(x, y)
+    #break statement
+    print("break statement")
+    i = 1
+    while i < 6:
+        print(i)
+        if i == 3:
+            break
+        i += 1
+        
+    fruits = ["apple", "banana", "cherry", "orange", "kiwi"]
 
-#break statement
-print("break statement")
-i = 1
-while i < 6:
-    print(i)
-    if i == 3:
-        break
-    i += 1
-    
-fruits = ["apple", "banana", "cherry", "orange", "kiwi"]
-
-for fruit in fruits:
-    if fruit == "cherry":
-        break
-    print(fruit)
-
-
-    
-#continue statement
-print("continue statement")
-i = 0
-while i < 6:
-    i += 1
-    if i == 3:
-        continue
-    print(i) 
-    
-fruits = ["apple", "banana", "cherry", "orange", "kiwi"]
-
-for fruit in fruits:
-    if fruit == "cherry":
-        continue
-    print(fruit)
+    for fruit in fruits:
+        if fruit == "cherry":
+            break
+        print(fruit)
 
 
-#range function
-print("range function")
-for x in range(6):
-    print(x)
+        
+    #continue statement
+    print("continue statement")
+    i = 0
+    while i < 6:
+        i += 1
+        if i == 3:
+            continue
+        print(i) 
+        
+    fruits = ["apple", "banana", "cherry", "orange", "kiwi"]
+
+    for fruit in fruits:
+        if fruit == "cherry":
+            continue
+        print(fruit)
+
+
+    #range function
+    print("range function")
+    for x in range(6):
+        print(x)
 
 ##Functions
+def functions():
+    def greet(name):
+        print("Hello, " + name + "!")
 
-def greet(name):
-    print("Hello, " + name + "!")
+    greet("Alice")
 
-greet("Alice")
+    def square(x):
+        return x * x
 
-def square(x):
-    return x * x
-
-print (square(4))
+    print (square(4))
 
 
 ##NOEL EXAMPLE TIME
 
 
-##Arrays in python
+##Arrays (lists) in python
+def arrays():
 
-fruits = ["apple", "banana", "cherry", "orange", "kiwi"]
+    fruits = ["apple", "banana", "cherry", "orange", "kiwi"]
 
-print(fruits[0])  # prints "apple"
-print(fruits[1])  # prints "banana"
+    print(fruits[0])  # prints "apple"
+    print(fruits[1])  # prints "banana"
 
-fruits[2] = "pear"  # replace "cherry" with "pear"
+    fruits[2] = "pear"  # replace "cherry" with "pear"
 
-fruits.append("grape")  # add "grape" to the end of the list
+    fruits.append("grape")  # add "grape" to the end of the list
 
-print(fruits)  # prints ["apple", "banana", "pear", "orange", "kiwi", "grape"]
+    print(fruits)  # prints ["apple", "banana", "pear", "orange", "kiwi", "grape"]
+
+
+#all the functions
+def main():
+    dataTypes()
+    #variables()
+    #operators()
+    #conditions()
+    #loops()
+    #functions()
+    #arrays()
+
+main()
